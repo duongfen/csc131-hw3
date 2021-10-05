280704924d7cd930438c183668446b5a825e3985
Gary Kane <garykane@csus.edu>
2021-10-04 18:28:20 -0700

    Looks students I did the homework too!

---
+++ code/participants.py
@@ -0,0 +1,14 @@
+# Welcome to Coding using Git! We all have to edit this same file.
+
+# Update the students list with your name in alphabetical order by first name.
+# Example: students = ['Fiz Ban', 'Foo bar']
+students = []
+professor = ['Gary Kane']
+course = ['CSC 131']
+
+print("%s's %s is the best!" % (professor[0], course[0]))
+print("Students: %s" % ', '.join(map(str, students)))
+
+# Run the program and make sure it works!
+# You can use a simple online IDE if you don't have a Python3
+# Example: https://ideone.com/l/python-3
---
+++ participants.md
@@ -0,0 +1,6 @@
+A list of participants in CSC 131
+---------------------------------
+
+*Please keep this list in alphabetical order by first name.*
+
+* Gary Kane
