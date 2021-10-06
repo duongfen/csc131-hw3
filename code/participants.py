4e5f687ad95fd5a3b0beaf3325b0a90fdba9ee7c
Gary Kane <garykane@csus.edu>
2021-10-06 13:32:23 -0700

    Removing sort because students should do this manually

--- code/participants.py
+++ code/participants.py
@@ -1,11 +1,11 @@
 # Welcome to Coding using Git! We all have to edit this same file.
 
 # Update the students list with your name in alphabetical order by first name.
+# This list will increase as more students complete the assignment.
 # Example: students = ['Fiz Ban', 'Foo bar']
 students = ['Vadym Kharchenko']
 professor = ['Gary Kane']
 course = ['CSC 131']
-students.sort()
 
 print("%s's %s is the best!" % (professor[0], course[0]))
 print("Students: %s" % ', '.join(map(str, students)))
