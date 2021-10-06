248bf0ea3f9d591800a9419b6c6eebe9dfd2575f
TylerColaizzi <tycolaizzi@gmail.com>
2021-10-06 16:11:34 -0700

    Added Tyler Colaizzi into participants.py

--- code/participants.py
+++ code/participants.py
@@ -3,7 +3,7 @@
 # Update the students list with your name in alphabetical order by first name.
 # This list will increase as more students complete the assignment.
 # Example: students = ['Fiz Ban', 'Foo bar']
-students = ['Connor Craig','Karen Xue','Steven Ho', 'Vadym Kharchenko']
+students = ['Connor Craig','Karen Xue','Steven Ho','Tyler Colaizzi','Vadym Kharchenko']
 professor = ['Gary Kane']
 course = ['CSC 131']
 
