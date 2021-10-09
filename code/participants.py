87a4f1194a5b406ec3b58e6614eba0c03f1bb50c
Andrew Xiong <92055475+Jinzoh@users.noreply.github.com>
2021-10-09 16:14:15 -0700

    Added Andrew Xiong to code list

--- code/participants.py
+++ code/participants.py
@@ -3,7 +3,7 @@
 # Update the students list with your name in alphabetical order by first name.
 # This list will increase as more students complete the assignment.
 # Example: students = ['Fiz Ban', 'Foo bar']
-students = ['Andrew Xiong','Arthur Gritsyuk','Connor Craig','Karen Xue', 'Lauren Prather','Miles Brown', 'Steven Ho','Tyler Colaizzi','Vadym Kharchenko','LJ Matias','Andrew Xiong']
+students = ['Andrew Xiong','Arthur Gritsyuk','Connor Craig','Karen Xue', 'Lauren Prather','Miles Brown', 'Steven Ho','Tyler Colaizzi','Vadym Kharchenko','LJ Matias']
 professor = ['Gary Kane']
 course = ['CSC 131']
 
