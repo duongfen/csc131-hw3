b16c92c6411fab114302f38772a69577a131a065
Jinzoh <andrewxiong@csus.edu>
2021-10-09 15:46:34 -0700

    Added Andrew Xiong to code/

--- code/participants.py
+++ code/participants.py
@@ -3,7 +3,7 @@
 # Update the students list with your name in alphabetical order by first name.
 # This list will increase as more students complete the assignment.
 # Example: students = ['Fiz Ban', 'Foo bar']
-students = ['Arthur Gritsyuk','Connor Craig','Karen Xue', 'Lauren Prather', 'Steven Ho','Tyler Colaizzi','Vadym Kharchenko','LJ Matias']
+students = ['Arthur Gritsyuk','Connor Craig','Karen Xue', 'Lauren Prather', 'Steven Ho','Tyler Colaizzi','Vadym Kharchenko','LJ Matias','Andrew Xiong']
 professor = ['Gary Kane']
 course = ['CSC 131']
 
